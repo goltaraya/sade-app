04e607c7c5e4e83d45848efea551635b4c6070d8
Yago Affonso <goltarayago@gmail.com>
2022-12-23 23:02:24 -0300

    implement: create colaborador

--- config/process.php
+++ config/process.php
@@ -8,6 +8,35 @@ include_once 'url.php';
 $data = $_POST;
 
 if (!empty($data)) {
+    $nome = $_POST['nome'];
+    $cpf = $_POST['cpf'];
+    $senha = $_POST['senha'];
+    $cargo = $_POST['cargo'];
+    $email = $_POST['email'];
+    $telefone = $_POST['telefone'];
+    $admin = $_POST['admin'];
+
+    $query = "INSERT INTO users 
+    (nome, cpf, senha, cargo, email, telefone, admin) 
+    VALUES 
+    (:nome, :cpf, :senha, :cargo, :email, :telefone, :admin)";
+    $stmt = $conn->prepare($query);
+    
+    $stmt->bindParam(":nome", $nome);
+    $stmt->bindParam(":cpf", $cpf);
+    $stmt->bindParam(":senha", $senha);
+    $stmt->bindParam(":cargo", $cargo);
+    $stmt->bindParam(":email", $email);
+    $stmt->bindParam(":telefone", $telefone);
+    $stmt->bindParam(":admin", $admin);
+
+    try {
+        $stmt->execute();
+    } catch (PDOException $e){
+        echo "Error:" . $e->getMessage();
+    }
+
+    header("Location: ". $BASE_URL . "../colaboradores.php");
 } else {
 
     if (!empty($_GET)) {
@@ -20,7 +49,6 @@ if (!empty($data)) {
         $stmt->bindParam(":id", $id);
         $stmt->execute();
         $colaborador = $stmt->fetch();
-
     } else {
         $colaboradores = [];
 
--- create-colaborador.php
+++ create-colaborador.php
@@ -2,14 +2,33 @@
 
 <div class="container">
     <h2 style="text-align: center;">Cadastro de colaborador</h2>
-    <form action="<?= $BASE_URL ?>/config/process.php" method="POST">
+    <form action="<?= $BASE_URL ?>config/process.php" method="POST">
         <div class="form-group">
             <p class="p-div">Nome</p>
-            <input type="text" class="form-control" name="nome" id="nomeColaborador" placeholder="Nome">
-        </div>
-        <div class="form-group">
+            <input class="input-group mb-3" type="text" class="form-control" name="nome" id="nomeColaborador">
+
             <p class="p-div">CPF</p>
-            <input type="text" class="form-control" name="cpf" id="cpfColaborador" placeholder="CPF"> 
+            <input class="input-group mb-3" type="text" class="form-control" name="cpf" id="cpfColaborador">
+
+            <p class="p-div">Senha</p>
+            <input class="input-group mb-3" type="text" class="form-control" name="senha" id="senhaColaborador">
+
+            <p class="p-div">Cargo</p>
+            <input class="input-group mb-3" type="text" class="form-control" name="cargo" id="cargoColaborador">
+
+            <p class="p-div">Email</p>
+            <input class="input-group mb-3" type="text" class="form-control" name="email" id="emailColaborador">
+
+            <p class="p-div">Telefone</p>
+            <input class="input-group mb-3" type="text" class="form-control" name="telefone" id="telefoneColaborador">
+
+            <p class="p-div">Este é um usuário administrador?</p>
+            <input type="radio" name="admin" value="1">
+            <label for="sim">Sim</label>
+            <input type="radio" name="admin" value="0">
+            <label for="não">Não</label>
+            <br>
+            <input type="submit" name="Submit" value="Enviar" style="align-self: center;">
         </div>
     </form>
 </div>
