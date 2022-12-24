1f899392b87bbb73703a51bfd9635d50ed1245a9
Yago Affonso <goltarayago@gmail.com>
2022-12-24 01:07:28 -0300

    init: edit colaborador

--- edit.php
+++ edit.php
@@ -3,7 +3,10 @@
 <div class="container">
 
     <h2 class="table-header">Editando perfil de <?= $colaborador["nome"] ?></h2>
-
+    <form action="config/process.php">
+        <label for="nome">Nome:</label>
+        <input type="text" placeholder="<?= $colaborador['nome'] ?>">
+    </form>
 </div>
 
 <?php include_once("templates/footer.php") ?>
